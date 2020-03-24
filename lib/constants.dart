51982f424388aa91245a0c6c1f9c593a97bcb432
Siew Wing Fei <siewwingfei@hotmail.com>
2020-03-24 17:13:45 +0800

    i

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,6 @@
 class Constants {
 
-  static const SERVER = 'http://192.168.0.188:8200';
+  static const SERVER = 'http://192.168.0.136:8200';
   static const PRODUCT_URL = '$SERVER/api/product';
   static const ORDER_URL = '$SERVER/api/order';
 }
